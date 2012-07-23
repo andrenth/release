3054dc71985c187b6ad6c2f39692625b6bcb3c49
Andre Nathan <andre@digirati.com.br>
2012-07-23 08:22:20 -0300

    Ditch the functorial interface to Release_config

--- lib/release_config.ml
+++ lib/release_config.ml
@@ -14,110 +14,104 @@ type section =
   | `Global of key list
   ]
 
-module type Ops = sig
-  val spec : section list
-end 
+type spec = section list
 
-module type S = sig
-  type t
+type t = (string, (string, value) Hashtbl.t) Hashtbl.t
 
-  val parse : Lwt_io.file_name -> [`Configuration of t | `Error of string]
-  val get : t -> ?section:string -> string -> unit -> value option
-  val reset : unit -> unit
-end
+let hash_find h k =
+  try Some (Hashtbl.find h k)
+  with Not_found -> None
 
-module Make(O : Ops) : S = struct
-  type t = (string, (string, value) Hashtbl.t) Hashtbl.t
+let validate_and cont validation value =
+  match Option.apply `Valid value validation with
+  | `Valid -> cont ()
+  | `Invalid r -> `Invalid r
 
-  let hash_find h k =
-    try Some (Hashtbl.find h k)
-    with Not_found -> None
-
-  let validate_and cont validation value =
-    match Option.apply `Valid value validation with
-    | `Valid -> cont ()
-    | `Invalid r -> `Invalid r
-
-  let rec validate_keys keys settings =
-    match keys with
-    | [] ->
-        `Valid
-    | key::rest ->
-        let keep_validating () =
-          validate_keys rest settings in
-        let missing_required_key k () =
-          `Invalid (sprintf "directive '%s' unspecified" k) in
-        let name, validation, deal_with_missing =
-          match key with
-          | `Required (k, v) -> k, v, missing_required_key k
-          | `Optional (k, v) -> k, v, keep_validating in
-        Option.either
-          deal_with_missing
-          (validate_and keep_validating validation)
-          (hash_find settings name)
-
-  let validate_keys_and cont keys settings =
-    match validate_keys keys settings with
-    | `Valid -> cont ()
-    | `Invalid r -> `Invalid r
-
-  let rec validate_sections sections conf =
-    match sections with
-    | [] ->
+let rec validate_keys keys settings =
+  match keys with
+  | [] ->
       `Valid
-    | section::sections ->
-        let keep_validating () =
-          validate_sections sections conf in
-        let missing_required_section s () =
-          `Invalid (sprintf "section '%s' missing" s) in
-        let name, keys, deal_with_missing =
-          match section with
-          | `Global ks -> Config.global_section, ks, keep_validating
-          | `Required (s, ks) -> s, ks, missing_required_section s
-          | `Optional (s, ks) -> s, ks, keep_validating in
-        Option.either
-          deal_with_missing
-          (validate_keys_and keep_validating keys)
-          (hash_find conf name)
+  | key::rest ->
+      let keep_validating () =
+        validate_keys rest settings in
+      let missing_required_key k () =
+        `Invalid (sprintf "directive '%s' unspecified" k) in
+      let name, validation, deal_with_missing =
+        match key with
+        | `Required (k, v) -> k, v, missing_required_key k
+        | `Optional (k, v) -> k, v, keep_validating in
+      Option.either
+        deal_with_missing
+        (validate_and keep_validating validation)
+        (hash_find settings name)
 
-  let validate =
-    validate_sections O.spec
+let validate_keys_and cont keys settings =
+  match validate_keys keys settings with
+  | `Valid -> cont ()
+  | `Invalid r -> `Invalid r
 
-  let join_with sep l =
-    List.fold_left (fun joined i -> joined ^ sep ^ (string_of_int i)) "" l
+let rec validate_sections conf spec =
+  match spec with
+  | [] ->
+    `Valid
+  | section::sections ->
+      let keep_validating () =
+        validate_sections conf sections in
+      let missing_required_section s () =
+        `Invalid (sprintf "section '%s' missing" s) in
+      let name, keys, deal_with_missing =
+        match section with
+        | `Global ks -> Config.global_section, ks, keep_validating
+        | `Required (s, ks) -> s, ks, missing_required_section s
+        | `Optional (s, ks) -> s, ks, keep_validating in
+      Option.either
+        deal_with_missing
+        (validate_keys_and keep_validating keys)
+        (hash_find conf name)
 
-  exception Error of string
+let validate =
+  validate_sections
 
-  let join_errors errors =
-    let concat msg (err, line) = msg ^ (sprintf "%s in line %d\n" err line) in
-    List.fold_left concat "" errors
+let join_with sep l =
+  List.fold_left (fun joined i -> joined ^ sep ^ (string_of_int i)) "" l
 
-  let parse file =
-    let ch = open_in file in
-    try
-      let lexbuf = Lexing.from_channel ch in
-      while true do
-        Config_parser.input Config_lexer.token lexbuf
-      done;
-      assert false (* not reached *)
-    with End_of_file ->
-      close_in ch;
-      match Config.errors () with
-      | [] ->
-          (try
-            let conf = Config.configuration in
-            match validate conf with
-            | `Valid -> `Configuration conf
-            | `Invalid reason -> `Error reason
-          with Error reason ->
-            `Error reason)
-      | errors ->
-          `Error (join_errors errors)
+exception Error of string
 
-  let get conf ?(section = Config.global_section) key () =
-    match hash_find conf section with
-    | Some settings -> hash_find settings key
-    | None -> None
+let join_errors errors =
+  let concat msg (err, line) = msg ^ (sprintf "%s in line %d\n" err line) in
+  List.fold_left concat "" errors
 
-  let reset = Config.reset
-end
+let parse file spec =
+  let ch = open_in file in
+  try
+    let lexbuf = Lexing.from_channel ch in
+    while true do
+      Config_parser.input Config_lexer.token lexbuf
+    done;
+    assert false (* not reached *)
+  with End_of_file ->
+    close_in ch;
+    match Config.errors () with
+    | [] ->
+        (try
+          let conf = Config.configuration in
+          match validate conf spec with
+          | `Valid -> `Configuration conf
+          | `Invalid reason -> `Error reason
+        with Error reason ->
+          `Error reason)
+    | errors ->
+        `Error (join_errors errors)
+
+let has_section conf section =
+  Hashtbl.mem conf section
+
+let get conf ?(section = Config.global_section) key () =
+  match hash_find conf section with
+  | Some settings -> hash_find settings key
+  | None -> None
+
+let get_exn conf ?(section = Config.global_section) key () =
+  Hashtbl.find (Hashtbl.find conf section) key
+
+let reset = Config.reset
--- lib_test/config_test.ml
+++ lib_test/config_test.ml
@@ -1,45 +1,43 @@
 open Printf
 open Release_config_types
 
+let path = "./lib_test"
+
 let validate_global_parameter = function
   | Int i ->
       if i = 0 || i = 1 then `Valid
       else `Invalid ("global_parameter must be a binary number")
   | _ -> `Invalid ("global_parameter must be a binary number")
 
-module ConfigOps = struct
-  let spec =
-    [ `Global
-        [ `Required ("global_parameter", Some validate_global_parameter)
-        ; `Optional ("another_global_parameter", None)
-        ]
-    ; `Required ("my-required-section",
-        [ `Required ("my-required-param", None)
-        ; `Optional ("my-optional-param", None)
-        ])
-    ; `Optional ("first-optional-section",
-        [ `Optional ("optional-parameter-1", None)
-        ; `Optional ("optional-parameter-2", None)
-        ])
-    ; `Optional ("second-optional-section",
-        [ `Required ("required-parameter-1", None)
-        ; `Required ("required-parameter-2", None)
-        ])
-    ]
-end
+let spec =
+  [ `Global
+      [ `Required ("global_parameter", Some validate_global_parameter)
+      ; `Optional ("another_global_parameter", None)
+      ]
+  ; `Required ("my-required-section",
+      [ `Required ("my-required-param", None)
+      ; `Optional ("my-optional-param", None)
+      ])
+  ; `Optional ("first-optional-section",
+      [ `Optional ("optional-parameter-1", None)
+      ; `Optional ("optional-parameter-2", None)
+      ])
+  ; `Optional ("second-optional-section",
+      [ `Required ("required-parameter-1", None)
+      ; `Required ("required-parameter-2", None)
+      ])
+  ]
 
-module Config = Release_config.Make(ConfigOps)
-
-let getg c k = Config.get c k ()
-let get c s k = Config.get c ~section:s k ()
+let getg c k = Release_config.get c k ()
+let get c s k = Release_config.get c ~section:s k ()
 
 let some_int i = (Some (Int i))
 let some_bool b = (Some (Bool b))
 let some_string s = (Some (String s))
 
 let () =
-  Config.reset ();
-  match Config.parse "./lib_test/complete.conf" with
+  Release_config.reset ();
+  match Release_config.parse (path ^ "/complete.conf") spec with
   | `Configuration conf ->
       assert (getg conf "global_parameter" = some_int 0);
       assert (getg conf "another_global_parameter" = some_bool true);
@@ -62,8 +60,8 @@ let () =
       assert false
 
 let () =
-  Config.reset ();
-  match Config.parse "./lib_test/missing-optional-values.conf" with
+  Release_config.reset ();
+  match Release_config.parse (path ^ "/missing-optional-values.conf") spec with
   | `Configuration conf ->
       assert (getg conf "global_parameter" = some_int 0);
 
@@ -78,19 +76,19 @@ let () =
       assert false
 
 let () =
-  Config.reset ();
-  match Config.parse "./lib_test/validation-error.conf" with
+  Release_config.reset ();
+  match Release_config.parse (path ^ "/validation-error.conf") spec with
   | `Configuration _ -> assert false
   | `Error err -> assert (err = "global_parameter must be a binary number")
 
 let () =
-  Config.reset ();
-  match Config.parse "./lib_test/missing-required-section.conf" with
+  Release_config.reset ();
+  match Release_config.parse (path ^ "/missing-required-section.conf") spec with
   | `Configuration _ -> assert false
   | `Error err -> assert (err = "section 'my-required-section' missing")
 
 let () =
-  Config.reset ();
-  match Config.parse "./lib_test/missing-required-param.conf" with
+  Release_config.reset ();
+  match Release_config.parse (path ^ "/missing-required-param.conf") spec with
   | `Configuration _ -> assert false
   | `Error err -> assert (err = "directive 'required-parameter-1' unspecified")
