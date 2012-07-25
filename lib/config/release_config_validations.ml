ebcdfc7a678342c9b97e4a2da27f0095c76fd44e
Andre Nathan <andre@digirati.com.br>
2012-07-25 12:47:47 -0300

    Better error messages

--- lib/config/release_config_validations.ml
+++ lib/config/release_config_validations.ml
@@ -25,8 +25,10 @@ let int_in_range (min, max) = function
 
 let string_matching re = function
   | `Str s ->
-      if Str.string_match (Str.regexp re) s 0 then `Valid
-      else `Invalid "string_matching: no match"
+      if Str.string_match (Str.regexp re) s 0 then
+        `Valid
+      else
+        `Invalid (sprintf "string_matching: %s doesn't match %s" s re)
   | _ ->
       `Invalid "string_matching: not a string"
 
@@ -35,9 +37,9 @@ let existing_file = function
       (try
         let st = Unix.lstat f in
         if st.Unix.st_kind = Unix.S_REG then `Valid
-        else `Invalid "existing_file: not a regular file"
+        else `Invalid (sprintf "existing_file: %s is not a regular file" f)
       with Unix.Unix_error (e, _, _) ->
-        `Invalid (sprintf "existing_file: %s" (Unix.error_message e)))
+        `Invalid (sprintf "existing_file: %s: %s" f (Unix.error_message e)))
   | _ -> `Invalid "existing_file: not a string"
 
 let existing_directory = function
@@ -45,9 +47,9 @@ let existing_directory = function
       (try
         let st = Unix.lstat f in
         if st.Unix.st_kind = Unix.S_DIR then `Valid
-        else `Invalid "existing_directory: not a directory"
+        else `Invalid (sprintf "existing_directory: %s is not a directory" f)
       with Unix.Unix_error (e, _, _) ->
-        `Invalid (sprintf "existing_file: %s" (Unix.error_message e)))
+        `Invalid (sprintf "existing_file: %s: %s" f (Unix.error_message e)))
   | _ -> `Invalid "existing_directory: not a string"
 
 let existing_basename = function
@@ -56,7 +58,7 @@ let existing_basename = function
         ignore (Unix.lstat (Filename.basename p));
         `Valid
       with Unix.Unix_error (e, _, _) ->
-        `Invalid (sprintf "existing_basename: %s" (Unix.error_message e)))
+        `Invalid (sprintf "existing_basename: %s %s" p (Unix.error_message e)))
   | _ -> `Invalid "existing_basename: not a string"
 
 let existing_user = function
@@ -64,18 +66,27 @@ let existing_user = function
       (try
         ignore (Unix.getpwnam u);
         `Valid
-      with Unix.Unix_error (e, _, _) ->
-        `Invalid (sprintf "existing_user: %s" (Unix.error_message e)))
+      with
+      | Unix.Unix_error (e, _, _) ->
+        `Invalid (sprintf "existing_user: %s: %s" u (Unix.error_message e))
+      | Not_found ->
+        `Invalid (sprintf "existing_user: %s: user not found" u))
   | _ -> `Invalid "existing_user: not a string"
 
 let one_of_ints l = function
   | `Int i ->
-      if List.mem i l then `Valid else `Invalid "one_of_ints: not found"
+      if List.mem i l then
+        `Valid
+      else
+        `Invalid (sprintf "one_of_ints: %d not found" i)
   | _ ->
       `Invalid "one_of_ints: not an int"
 
 let one_of_strings l = function
   | `Str s ->
-      if List.mem s l then `Valid else `Invalid "one_of_strings: not found"
+      if List.mem s l then
+        `Valid
+      else
+        `Invalid (sprintf "one_of_strings: %s not found" s)
   | _ ->
       `Invalid "one_of_strings: not a string"
