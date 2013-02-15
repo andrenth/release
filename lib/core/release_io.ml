2bc17e18eb5bc05d42de763bc8004a3fbf4b069f
Andre Nathan <andre@digirati.com.br>
2013-02-15 16:19:13 -0200

    Fix memory leak in Release_io
    Bring commits 354b13e..1984516 from master.

--- lib/core/release_io.ml
+++ lib/core/release_io.ml
@@ -1,5 +1,7 @@
 open Lwt
 
+module O = Release_util.Option
+
 let rec interrupt_safe f =
   try_lwt
     f ()
@@ -15,29 +17,29 @@ let read_once fd buf offset n =
   eintr_safe Release_buffer.read fd buf offset n
 
 let read ?(timeout) fd n =
-  let buf = Release_buffer.create n in
-  let rec read offset remain =
+  let rec read_into buf offset remain =
     if remain = 0 then
       return offset
     else
       lwt k = read_once fd buf offset remain in
-      read (offset + k) (if k = 0 then 0 else remain - k) in
-  let timeout = match timeout with None -> infinity | Some t -> t in
-  let timeout_t = (* XXX doesn't this raise??? *)
-    lwt () = Lwt_unix.timeout timeout in
-    return `Timeout in
-  let read_t =
-    match_lwt read 0 n with
+      read_into buf (offset + k) (if k = 0 then 0 else remain - k) in
+  let handle_read () =
+    let buf = Release_buffer.create n in
+    match_lwt read_into buf 0 n with
     | 0 -> return `EOF
-    | k -> return (`Data (Release_buffer.sub buf 0 k)) in
-  timeout_t <?> read_t
+    | k -> return (`Data (if k = n then buf else Release_buffer.sub buf 0 k)) in
+  let read_with_timeout t =
+    let timeout_t =
+      lwt () = Lwt_unix.sleep t in
+      return `Timeout in
+    Lwt.pick [timeout_t; handle_read ()] in
+  O.either handle_read read_with_timeout timeout
 
 let write fd buf =
-  let len = Release_buffer.length buf in
   let rec write offset remain =
     if remain = 0 then
       return_unit
     else
       lwt k = eintr_safe Release_buffer.write fd buf offset remain in
       write (offset + k) (if k = 0 then 0 else remain - k) in
-  write 0 len
+  write 0 (Release_buffer.length buf)
